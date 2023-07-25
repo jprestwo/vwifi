30b71b89d3228be89dbf4128c748b1c46e9f7168
James Prestwood <prestwoj@gmail.com>
2023-07-25 07:19:39 -0700

    Include port in IP hash

--- src/tools.cc
+++ src/tools.cc
@@ -4,6 +4,7 @@
 #include "tools.h"
 
 #include <assert.h> // assert
+#include <stdio.h>
 
 unsigned long hash_ipaddr(struct sockaddr_in* addr)
 {
@@ -11,10 +12,12 @@ unsigned long hash_ipaddr(struct sockaddr_in* addr)
 
 	assert( addr != NULL );
 
+	//printf("PORT: %u\n", addr->sin_port);
+
 	res = (((addr->sin_addr.s_addr >> 24) & 0xff) * 256)	+
 			(((addr->sin_addr.s_addr >> 16) & 0xff) * 256)	+
 			(((addr->sin_addr.s_addr >>  8) & 0xff)* 256)	+
-			(addr->sin_addr.s_addr & 0xff);
+			(addr->sin_addr.s_addr & 0xff) + addr->sin_port;
 
 	return res;
 }
